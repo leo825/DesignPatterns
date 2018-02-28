0f366c6ade77535d4f8d71d519dfe09e2810076e
leo825 <294864460@qq.com>
2018-02-28 11:22:32 +0800

    添加了三种结构类型模式 1.适配器模式 2.装饰器模式 3.代理模式

--- src/com/designpatterns/structural/Adapter/class_/Adapter.java
+++ src/com/designpatterns/structural/Adapter/class_/Adapter.java
@@ -1,9 +1,8 @@
-package com.designpatterns.structural.Adapter;
+package com.designpatterns.structural.Adapter.class_;
 /**
  * ClassName: Adapter.java
  * @Description: 类的适配器模式
- * 核心思想就是：有一个Source类，拥有一个方法，待适配，目标接口时Targetable，
- * 通过Adapter类，将Source的功能扩展到Targetable里
+ * 核心思想就是：有一个Source类，拥有一个方法，待适配，目标接口时Targetable，通过Adapter类，将Source的功能扩展到Targetable里
  * @author leo
  * @date 2018年2月28日 上午12:11:45
  * @modifier
---
+++ src/com/designpatterns/structural/Adapter/class_/Source.java
@@ -0,0 +1,15 @@
+package com.designpatterns.structural.Adapter.class_;
+/**
+ * ClassName: Source.java
+ * @Description: Source类
+ * @author leo
+ * @date 2018年2月27日 下午11:55:19
+ * @modifier
+ * @modify-date 2018年2月27日 下午11:55:19
+ * @version 1.0
+ */
+public class Source {
+	public void method1() {  
+        System.out.println("this is original method!");  
+    }
+}
